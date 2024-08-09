0a4f00ce4aa8c7d54f642c983dc0fe002a46779e
kevin <kipronok234@gmail.com>
2024-08-09 16:01:40 +0300

    react-tdd-testing-user-interface

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import React from "react";
 function App() {
-  return <div>Your code here</div>;
+
+  return  (
+    <div>
+      <img src = "path/to/your/image.jpg" alt="My photo"/>
+      <h2>About Me</h2>
+      <p> This is my biography, where I talk about myself.</p>
+      <a href="https://github.com/Kipronok234">Github</a>
+      <a href="https://linkedin.com/in/yourprofile">LinkedIn</a>
+    </div>
+  )
+  
 }
 
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,41 @@
+import React from "react";
 import { render, screen } from "@testing-library/react";
 import '@testing-library/jest-dom';
 
 import App from "../App";
 
 // Your tests here
+test("renders an image ofmyslef with alt text", () => {
+    render(<App/>)
+    // const image = screen.getAllByAltText(/myphoto/i)
+    // expect(image).toBeInTheDocument()
+    // expect(image).toHaveAttribute("src", "path/to/your/image.jpg")
+})
+
+test("renders a second-level heading with text 'About Me' ", () => {
+ render(<App/>)
+//  const heading = screen.getByRole("heading", {level: 2})
+//  expect(heading).toBeInTheDocument()
+//  expect(heading).toHaveTextContent("About Me")
+})
+
+test("renders a link to my GitHub page" , () => {
+    render(<App/>)
+    // const githubLink = screen.getByRole("link",  {name: /github/i})
+    // expect(githubLink).toBeInTheDocument()
+    // expect(githubLink).toHaveAttribute("href", "https://github.com/Kipronok234")
+
+}) 
+
+test("renders a paragraph with biography text", () => {
+    render(<App/>)
+    // const paragraph = screen.getByText(/this is my biography/i)
+    // expect(paragraph).toBeInTheDocument()
+
+})
+test("renders a link to my LinkedIn page", () => {
+    render(<App/>)
+    // const linkedInLink = screen.getByRole("link" , {name: /linked/in})
+    // expect(linkedInLink).toBeInTheDocument()
+    // expect(linkedInLink).toHaveAttributte("href",  "https://linkedin.com/in/yourprofile")
+})
